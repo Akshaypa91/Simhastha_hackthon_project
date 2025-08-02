415924e7081d07b1a72196f40b47ac9434908528
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2025-08-02 11:42:47 +0530

    NavBar is updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,9 +3,11 @@ import React, { useState } from 'react';
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     return(
-        <nav className="bg-green-600 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-            
+        <nav className="bg-orange-600 p-4">
+            <div className="container mx-auto flex items-start justify-end space-x-10 text-white">
+                <a href="">Sponsers</a>
+                <a href="">About</a>
+                <a href="">Login/SignUp</a>
             </div>
         </nav>
     )
