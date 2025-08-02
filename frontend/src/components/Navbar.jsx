5478893d5961d3769a7b86a7b9e3f6c1b548de61
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2025-08-02 10:53:38 +0530

    frontend is started

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    return(
+        <nav className="bg-green-600 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+            
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
